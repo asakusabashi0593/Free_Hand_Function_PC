6530e49497a3a7ed3c45cb34f8c1cbbf43fd8df5
Koki1111a <ak20011228@gmail.com>
2024-09-15 17:22:06 +0900

    explain functions in screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -8,6 +8,8 @@ from copy import deepcopy
 
 def show(img):
     '''画像の表示
+    Args:
+        img (image): 描画したい画像
     '''
     plt.imshow(img, vmin=0, vmax=255)
     plt.show()
@@ -18,6 +20,10 @@ def show(img):
 
 def get_screen(region=None):
     '''プレイエリアキャプチャ
+    Args:
+        region (taple): スクショしたい領域(x, y, +x, +y)
+    Returns:
+        img (image): cv2系の画像データ
     '''
     img = pyautogui.screenshot(region=region) #PIL系でキャプチャされる
     return np.array(img, 'uint8') #cv2系で返す
